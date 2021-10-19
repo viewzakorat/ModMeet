c4464f5102a8c114ad23ddd6e38dd0349a0638a0
JTraisit <Traisit.john7215@mail.kmutt.ac.th>
2021-10-19 16:41:13 +0700

    Second commit clip1

--- app/src/main/java/com/example/modvideoll/MainActivity.java
+++ app/src/main/java/com/example/modvideoll/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.modvideoll;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/modvideoll/SignUpActivity.java
+++ app/src/main/java/com/example/modvideoll/SignUpActivity.java
@@ -1,24 +1,22 @@
 package com.example.modvideoll;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 
-public class SignInActivity extends AppCompatActivity {
-    Button ButtonSignUp;
+public class SignUpActivity extends AppCompatActivity {
+    Button  ButtonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_sign_in);
-        ButtonSignUp = findViewById(R.id.ButtonSignUp);
-        ButtonSignUp.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_sign_up);
+        ButtonSignIn = findViewById(R.id.ButtonSignIn);
+        ButtonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+                startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
             }
         });
 
